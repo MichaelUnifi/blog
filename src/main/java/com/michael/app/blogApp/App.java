d26a94c8addca6598da1ed83bbfc3bad8c8a92a5
MichaelUnifi <michael.bartoloni@edu.unifi.it>
2025-11-24 15:20:28 +0100

    first commit

---
+++ src/main/java/com/michael/app/blogApp/App.java
@@ -0,0 +1,13 @@
+package com.michael.app.blogApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
